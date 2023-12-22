8ff9d1de4ad75bb435eff1d4f9d5a57dd2b43776
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-12-22 16:27:21 +0000

    Finished navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import Context from "../store/appContext";
 
 export const Navbar = () => {
+  const { store, actions } = useContext(Context);
+
   return (
-    <div className="">
-      <nav className="navbar navbar-expand-lg navbar-secondary" style={{ backgroundColor: "#F7F7F7" }}>
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"><img src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" alt="StarWars Logo" width="120" height="120" style={{ marginLeft: "35px" }} /></a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item dropdown ms-auto">
-                <button className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                  Favorites
-                </button>
-                <ul className="dropdown-menu dropdown-menu-end dropdown-menu-dark">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar navbar mb-3 mt-3 justify-content-evenly navbar-secondary" style={{ backgroundColor: "#F7F7F7" }}>
+      <Link to="/">
+        <img className="w-25" src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" alt="star wars logo" />
+        <button className="btn btn-outline-success border-3">Go Home</button>
+      </Link>
+      <div className="dropdown">
+        <button className="btn btn-outline-success border-3 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+          Favorites
+          <span className="border border-success rounded-pill ms-2 p-1">
+            <strong>{store.favorites.length}</strong>
+          </span>
+        </button>
+
+        <ul className="dropdown-menu dropdown-menu-end" aria-aria-labelledby="dropdownMenuButton1">
+          {(store.favorites && store.favorites.length > 0) ?
+            store.favorites.map((item, index) => (
+              <Link to={item && item.path ? ("/" + item.path + "/" + item.index) : ""} style={{ textDecoration: "none" }}>
+                <li key={item}>
+                  <a className="dropdown-item d-flex justify-content-between">{item}<i className="trash fas fa-trash-alt mt-1 ms-3" onClick={() => actions.removeFavorite(index)}></i></a>
+                </li>
+              </Link>
+            ))
+            : <li className="text-center text-muted">No Favorites</li>}
+        </ul>
+      </div>
+    </nav>
   );
 };
