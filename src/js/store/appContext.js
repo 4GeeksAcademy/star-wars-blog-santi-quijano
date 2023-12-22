795080cc89b7b776436e9d69f9b99219a7f215b0
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-12-22 15:56:46 +0000

    Finished appcontext

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -32,6 +32,8 @@ const injectContext = PassedComponent => {
 			 *
 			 **/
 			state.actions.getPeople();
+			state.actions.getPlanets();
+			state.actions.getVehicles();
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
